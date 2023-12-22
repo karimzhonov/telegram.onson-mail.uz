24f9b9eddee1f4094dff7070349dc95cb17c1ceb
x.karimjonov <khtkarimzhonov@gmail.com>
2023-12-22 12:32:08 +0500

    add all orders button

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -53,12 +53,12 @@ async def _menu_keyboard(msg: types.Message, user_id=None):
         keyboard.row(types.KeyboardButton(text=_(LISTPASSPORT, msg.bot.lang)))
     else:
         keyboard.row(types.KeyboardButton(text=_(TAKE_ID, msg.bot.lang)))
+    keyboard.row(types.KeyboardButton(text=_(ACCPET_BUTTON, msg.bot.lang)), types.KeyboardButton(text=_(STORAGES, msg.bot.lang)), types.KeyboardButton(text=_(FOTO_REPORTS, msg.bot.lang)))
     if await ClientId.objects.filter(user_id=user_id, deleted=False).aexists():
         clientid = await ClientId.objects.filter(user_id=user_id, deleted=False).afirst()
-        keyboard.row(types.KeyboardButton(text=_(ACCPET_BUTTON, msg.bot.lang)), types.KeyboardButton(text=_(STORAGES, msg.bot.lang)), types.KeyboardButton(text=f"{_(ORDERS, msg.bot.lang)} ({await Order.objects.filter(with_online_buy=False, client__in=clientid.clients.all()).acount()})"))
+        keyboard.row(types.KeyboardButton(text=_(ONLINE_BUY, msg.bot.lang)), types.KeyboardButton(text=f"{_(ORDERS, msg.bot.lang)} ({await Order.objects.filter(with_online_buy=False, client__in=clientid.clients.all()).acount()})"))
     else:
-        keyboard.row(types.KeyboardButton(text=_(ACCPET_BUTTON, msg.bot.lang)), types.KeyboardButton(text=_(STORAGES, msg.bot.lang)), types.KeyboardButton(text=_(ORDERS, msg.bot.lang)))
-    keyboard.row(types.KeyboardButton(text=_(ONLINE_BUY, msg.bot.lang)), types.KeyboardButton(text=_(FOTO_REPORTS, msg.bot.lang)))
+        keyboard.row(types.KeyboardButton(text=_(ONLINE_BUY, msg.bot.lang)), types.KeyboardButton(text=_(ORDERS, msg.bot.lang)))
     keyboard.row(types.KeyboardButton(text=_(SETTINGS, msg.bot.lang)), types.KeyboardButton(text=_(INFO, msg.bot.lang)), types.KeyboardButton(text=_(CALCULATOR, msg.bot.lang)))
     if await ClientId.objects.filter(user_id=user_id).aexists():
         keyboard.row(types.KeyboardButton(text=_(FAQ, msg.bot.lang)), types.KeyboardButton(text=_(EXIT, msg.bot.lang)))
