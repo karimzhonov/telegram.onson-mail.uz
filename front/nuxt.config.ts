b2b28d3a81078391e95fc60bebd71f8280b6dbc6
x.karimjonov <khtkarimzhonov@gmail.com>
2025-02-03 12:17:38 +0500

    add download button

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,14 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default {
     ssr: false,
     modules: [
         '@nuxtjs/color-mode',
         '@nuxt/image',
     ],
-    css:[
+    css: [
         '~/assets/css/app.css'
     ],
-    colorMode:{
+    colorMode: {
         preference: 'system', // default value of $colorMode.preference
         fallback: 'dark', // fallback value if not system preference found
         hid: 'nuxt-color-mode-script',
@@ -18,18 +18,36 @@ export default defineNuxtConfig({
         classSuffix: '',
         storageKey: 'nuxt-color-mode'
     },
-    postcss:{
+    postcss: {
         plugins: {
             tailwindcss: {},
             autoprefixer: {}
         }
     },
     vite: {
-      server: {
-        fs: {
-          // Allow serving files from one level up to the project root
-          allow: ['/'],
+        server: {
+            fs: {
+                // Allow serving files from one level up to the project root
+                allow: ['/'],
+            },
         },
-      },
     },
-})
+    buildModules: [
+        '@nuxtjs/pwa',
+    ],
+    pwa: {
+        manifest: {
+            name: 'Onson Mail',
+            short_name: 'Onson Mail',
+            lang: 'ru',
+            description: 'Onson Mail Group',
+        },
+        icon: {
+            source: '/logo.png',
+            fileName: 'logo.png',
+        },
+        meta: {
+            mobileAppIOS: true
+        }
+    }
+}
